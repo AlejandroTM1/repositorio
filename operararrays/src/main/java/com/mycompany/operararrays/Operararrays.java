9f196917cccbdf83d2534136e951a0e7e28cddc6
Alejandro <alextm124@gmail.com>
2026-04-14 12:12:10 +0200

    Se añade rellenaArrays

---
+++ operararrays/src/main/java/com/mycompany/operararrays/Operararrays.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operararrays;
+
+/**
+ *
+ * @author admin
+ */
+public class Operararrays {
+
+    public static void main(String[] args) {
+       int [] hola = new int[10];
+       
+       array hola2 = new array();
+       
+       
+       hola2.rellenaArrays(hola);
+       
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hola[i]);
+        }
+    }
+}
---
+++ operararrays/src/main/java/com/mycompany/operararrays/array.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.operararrays;
+
+/**
+ *
+ * @author admin
+ */
+public class array {
+    
+    public int[] rellenaArrays(int[] array){
+            for (int i = 0; i < array.length; i++) {
+                array[i]=(int)(Math.random()*19)+1;
+            }
+            
+            return array;
+    
+}
+}
